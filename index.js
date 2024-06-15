dc92126589c3918208b848bdd20f0bae66db268c
Wendell <zaquielriggs@gmail.com>
2024-06-15 12:47:05 -0300

    inicializei o projeto

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const hbs = require("express-handlebars");
+const PORT = process.env.PORT || 3000;
+
+
+//conf do handlebars
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main'
+})); app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index', { NavActiveCad: true });
+})
+
+app.get('/users', (req, res) => {
+    res.render('users', { NavActiveUsers: true });
+})
+app.get('/editar', (req, res) => {
+    res.render('editar', { NavActiveUsers: true });
+})
+
+
+
+
+app.listen(PORT, () => {
+    console.log('Servidor rodando em http://localhost:' + PORT);
+})
